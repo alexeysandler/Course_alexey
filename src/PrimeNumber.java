967a9735d52a15532a35f9db6e614048d9b3a0c7
Alexey Sandler <as371x@att.com>
2018-03-09 13:01:12 +0200

    My first push to branch1 cherry pick

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -25,7 +25,7 @@ public class PrimeNumber {
                 //if not, then just check the odds
                 for(int i=3;i*i<=n;i+=2) {
                     if(n%i==0)
-                        return false;
+                        return false ;
                 }
                 return true;
             }
--- src/lesson_4_03_2017/Fibonacci_2.java
+++ src/lesson_4_03_2017/Fibonacci_2.java
@@ -2,7 +2,7 @@ package lesson_4_03_2017;
 
 public class Fibonacci_2{
     public static void main(String[] args) {
-        System.out.println( fibonacci(6));
+        System.out.println( "ss" +fibonacci(6));
     }
 
         private static int fibonacci(int i) {
@@ -12,7 +12,7 @@ public class Fibonacci_2{
 
         private static int[] fibonacci2(int i) {
     if(i <=1) {
-        return new int[]{1, 1};
+        return new int[]{1, 1} ;
     }
              int [] arr = fibonacci2(i-1);
     return new int[] {arr[1],arr[0]+arr[1]};
