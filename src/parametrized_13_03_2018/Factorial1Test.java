1370d047ca6c0c67fd3906c7e1f220a763f23365
Alexey Sandler <as371x@att.com>
2018-03-16 08:52:06 +0200

    Try to resolve the

---
+++ src/parametrized_13_03_2018/Factorial1Test.java
@@ -0,0 +1,44 @@
+package parametrized_13_03_2018;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.runners.Parameterized.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.junit.Assert.*;
+@RunWith(Parameterized.class)
+
+public class Factorial1Test {
+    private int input;
+    private int output;
+
+    public Factorial1Test(int input, int output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    @Test
+    public void factorialMultiTest(){
+
+        assertEquals(Factorial1.calcFactorial(input), output);
+
+    }
+
+    @Parameters
+    public static Collection<Object[]> data(){
+        return Arrays.asList(new Object[][]{
+                {5,120},
+                {2,2},
+                {3,6},
+        });
+    }
+
+
+
+
+
+}
